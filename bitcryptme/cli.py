b84280561db0d3b0951b6d831cafe37aa7a691f2
awaisjafar <awaisjafar@users.noreply.github.com>
2022-10-28 07:22:16 +0000

    ✅ Ready to clone and code.

--- bitcryptme/cli.py
+++ bitcryptme/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for bitcryptme project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m bitcryptme` and `$ bitcryptme `.
 
     This is your program's entry point.
 
